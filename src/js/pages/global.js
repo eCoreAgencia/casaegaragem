451c9c934c181c938df9bc79ded5ed8ffc6d7969
ronaldjunger <ronaldjunger@gmail.com>
2019-05-02 16:46:47 -0300

    ajuste titulo busca

--- src/js/pages/global.js
+++ src/js/pages/global.js
@@ -6,14 +6,22 @@ class Global{
     }
 
     renameTitle() {
-        var urlBusca = window.location.href;
-        var nomeDaColecao = document.querySelector(".search__word");
-        var buscaPor = document.querySelectorAll(".search__title");
-    
-        if(urlBusca.indexOf("?fq=H:") != -1){
-            nomeDaColecao.textContent = document.title;
-            buscaPor.style.display = "none";
+        let titleUrl = window.location.href;
+
+        if(titleUrl.indexOf('&colecao=')) {
+            let title = titleUrl.split('&colecao=')[1];
+
+            $('.search-word .search__word').text(title.replace(/%20/g, ' ').replace(/%C3%A7/g, 'ç').replace(/%C3%AA/g, 'ê'));
         }
+
+        // var urlBusca = window.location.href;
+        // var nomeDaColecao = document.querySelector(".search__word");
+        // var buscaPor = document.querySelectorAll(".search__title");
+    
+        // if(urlBusca.indexOf("?fq=H:") != -1){
+        //     nomeDaColecao.textContent = document.title;
+        //     buscaPor.style.display = "none";
+        // }
     }
 }
 
